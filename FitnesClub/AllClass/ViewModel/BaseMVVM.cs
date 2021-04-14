90e0db0ffee6e6db1e94269b795cfeb0d79ca073
lalal_96@list.ru <lalal_96@list.ru>
2021-04-14 11:43:37 +0600

    11..43

---
+++ FitnesClub/AllClass/ViewModel/BaseMVVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FitnesClub.AllClass.MVVM
+{
+    abstract internal class BaseMVVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        virtual protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        virtual protected bool Set<T>(ref T ppp, T value, [CallerMemberName] string nn = "")
+        {
+
+            if (Equals(ppp, value)) return false;
+            ppp = value;
+            OnPropertyChanged(nn);
+            return true;
+        }
+    }
+}
---
+++ FitnesClub/AllClass/ViewModel/MVVMButtonCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace FitnesClub.AllClass.MVVM
+{
+    class MVVMButtonCommand : ICommand
+    {
+        private Action<object> _action;
+        public MVVMButtonCommand(Action<object> action)
+        {
+            _action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            if (parameter != null)
+            {
+                _action(parameter);
+            }
+            else
+            {
+                _action("-1");
+            }
+        }
+        public event EventHandler CanExecuteChanged;
+    }
+}
